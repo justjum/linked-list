39eb07859006349f3bc8f5e3e0dda3a5e8eb02ed
justjum <jumasaurusrex@gmail.com>
2023-09-22 19:53:13 +0930

    Add class methods for search and indexed add/rem - Completed functions to:   - search   - search & return index   - add at index   - remove value at index

--- script.js
+++ script.js
@@ -72,12 +72,76 @@ class linkedList {
 
     contains(value) {
         let current = this.first;
-        for (let x=0; x<this.entries; x++) {
+        while (current.value) {
             console.log(current.value);
-            current.value === value ? console.log(true): console.log(false);
+            if (current.value === value) {
+                return console.log(true);
+            }
+            else if (current.nextNode === null) {
+                return console.log(false);
+
+            }
+            current = current.nextNode;
+            
+        }  
+        
+    }
+
+    find(value) {
+        let current = this.first;
+        let index = 0;
+        while (current.value) {
+            if (current.value === value) {
+                return console.log(`Index ${index}`);
+            }
+            else if (current.nextNode === null) {
+                return console.log(null);
+            }
+            current = current.nextNode;
+            index++;
         }
     }
 
+    toString() {
+        let current = this.first;
+        let str = current.value;
+        while (current.nextNode) {
+            current = current.nextNode;
+            str = str.concat(...' -> ', current.value)
+            
+        }
+        str = str.concat(...' -> ', null);
+        return console.log(str);
+    }
+
+    insertAt(value, index) {
+        let current = this.first;
+        for (let x=0; x<index-1; x++) {
+            current = current.nextNode;
+        }
+        let temp = current.nextNode;
+        current.nextNode = new node(value, temp);
+    }
+
+    removeAt(index) {
+        let current = this.first;
+        if (index > this.entries) {
+            console.log("Empty index");
+        }
+        else {
+            for (let x=0; x<index-1; x++) {
+                current = current.nextNode
+            }
+            let temp = current.nextNode;
+            console.log(`Remove ${temp.value} at index ${index}`)
+            temp = temp.nextNode;
+            current.nextNode = temp;
+            
+        }
+
+
+
+    }
 
 }
 
@@ -89,10 +153,11 @@ class node {
 
 }
 
-let list = new linkedList("Party");
+let list = new linkedList();
 list.append("tipple");
 list.append("whittle");
 list.append("trigger");
+list.prepend('second');
 list.prepend("first");
 list.size();
 list.head();
@@ -104,3 +169,12 @@ list.pop();
 list.tail();
 console.table(list);
 list.contains('whittle');
+list.contains('greg');
+list.find('first');
+list.find('tipple');
+list.find('trigger'); //had been deleted with earlier 'pop'
+list.toString();
+list.insertAt('bubble', 3);
+list.toString();
+list.removeAt(3);
+list.toString();
